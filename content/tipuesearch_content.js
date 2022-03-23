ed7bb82384fd66bd299f1983cdc908fe63b1d6d4
40923107 <40923107@gm.nfu.edu.tw>
2022-03-23 09:46:24 +0800

    123

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n', 'tags': '', 'url': 'About.html'}, {'title': '每周討論', 'text': '', 'tags': '', 'url': '每周討論.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n 123123123123 \n', 'tags': '', 'url': 'About.html'}, {'title': '每周討論', 'text': '', 'tags': '', 'url': '每周討論.html'}]};
