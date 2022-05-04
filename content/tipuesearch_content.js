bbf9ce5ec802413f6609f7a74c31824abe1b8442
40923107 <40923107@gm.nfu.edu.tw>
2022-05-04 09:37:47 +0800

    每周討論

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,2 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n \n test \n', 'tags': '', 'url': 'About.html'}, {'title': '每周討論', 'text': '', 'tags': '', 'url': '每周討論.html'}]};
-
+var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n \n test \n', 'tags': '', 'url': 'About.html'}, {'title': '每周討論', 'text': '哇哈哈', 'tags': '', 'url': '每周討論.html'}]};
