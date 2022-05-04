7618ff8a52ef72eaa7e127076f0b612aa62644c0
40923105 <4023105@gm.nfu.edu.tw>
2022-05-04 09:50:35 +0800

    886

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n twewrwwewe \n jiwjti4t4 \n 886', 'tags': '', 'url': 'About.html'}]};
