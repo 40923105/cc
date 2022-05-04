56066fe4a86c1a380059e845c30f74ff2a68784a
40923105 <4023105@gm.nfu.edu.tw>
2022-05-04 09:47:18 +0800

    c8 c8

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n 黑呦 \n', 'tags': '', 'url': 'About.html'}, {'title': '每周討論', 'text': '', 'tags': '', 'url': '每周討論.html'}]};
-=======
-var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n \n test \n', 'tags': '', 'url': 'About.html'}, {'title': '每周討論', 'text': '', 'tags': '', 'url': '每周討論.html'}]};
-
->>>>>>> 4322019be6cc027d1ec290e4b227cc9b8f48480d
+var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n', 'tags': '', 'url': 'About.html'}]};
