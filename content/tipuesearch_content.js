1aa104e14f16f1230839642ec72769bca678940d
40923107 <40923107@gm.nfu.edu.tw>
2022-05-04 09:50:49 +0800

    哈哈

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '爛透了 \n \n 哇哈哈~~~~~~~~~~ \n e04i u6un6ru \n \n 05/04哈囉 \n 1321321 \n ccccccccccccccccccccccccccccccc \n 哈哈哈哈哈 \n 哈哈哈哈 \n 哈哈哈 \n 哈哈 \n', 'tags': '', 'url': 'About.html'}]};
